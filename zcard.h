cd6008ae77a3faae5a012d44ec0b2b95cffb142f
Clearlove101 <66292873+Clearlove101@users.noreply.github.com>
2020-06-14 17:05:28 +0800

    Add files via upload

---
+++ zcard.h
@@ -0,0 +1,58 @@
+#ifndef ZCARD_H
+#define ZCARD_H
+
+#include <QLabel>
+#include <QMovie>
+#include <QSound>
+#include <QMouseEvent>
+
+#include "zObject.h"
+#include "zScene.h"
+
+class zCard : public zObject
+{
+public:
+    zCard(QWidget* parent = 0);
+    ~zCard();
+    int plantIndex;
+    int sunPoint = 50;
+    int frame_max = 1, frame = 1;
+    virtual void act();
+    QWidget* front;
+    QWidget* back;
+    QLabel* frontText;
+    void setIndex(int index);
+    void transFront();
+protected:
+    void mousePressEvent(QMouseEvent* event);
+};
+
+class zSunFlowerCard : public zCard
+{
+public:
+    zSunFlowerCard(QWidget* parent = 0);
+    ~zSunFlowerCard();
+private:
+    QMovie* anim = new QMovie(":/Cards/card_Sunflower.png");
+};
+
+class zPeaShooterCard : public zCard
+{
+public:
+    zPeaShooterCard(QWidget* parent = 0);
+    ~zPeaShooterCard();
+private:
+    QMovie* anim = new QMovie(":/Cards/card_PeaShooter.png");
+};
+
+class zWallNutCard : public zCard
+{
+public:
+    zWallNutCard(QWidget* parent = 0);
+    ~zWallNutCard();
+private:
+    QMovie* anim = new QMovie(":/Cards/card_WallNut.png");
+};
+
+
+#endif // ZCARD_H
